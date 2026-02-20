5cf163ea6852b3ccf07adb93f649fd293ad3f3e7
abdelra7man <abdelra7manabdullah@gmail.com>
2026-02-20 17:13:51 +0200

    Adding New Form Request For Login user and adding Login logic

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\RegisterRequest;
+use App\Http\Requests\SessionRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
@@ -15,9 +17,18 @@ class AuthController extends Controller
 
     }
 
-    public function login(Request $request)
+    public function login(SessionRequest $request)
     {
-        // Login logic would go here
+    
+        // Tring To login with user credentials and return token if successful
+            $credentials = $request->only('email', 'password');
+            if (Auth::attempt($credentials)) {
+                $user = Auth::user();
+                $token = $user->createToken('auth_token_'.$user->id)->plainTextToken;
+                return response()->json(['access_token' => $token, 'token_type' => 'Bearer']);
+            }
+            return response()->json(['message' => 'Invalid credentials'], 401);
+
     }
 
     public function logout(Request $request)
